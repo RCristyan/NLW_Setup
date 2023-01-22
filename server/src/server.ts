f6205abc062ea565a3f55fbbd073285c9ef97c1b
RCristyan <rcristyan9@gmail.com>
2023-01-22 17:26:53 -0300

    Created routes file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,11 @@
 import Fasfity from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from './routes'
 
 const app = Fasfity()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/habit', async () => {
-    const habits = await prisma.habit.findMany()
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333
