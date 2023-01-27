0d242e60cd4c36e321868b446bf73afb1cb26870
RCristyan <rcristyan9@gmail.com>
2023-01-27 18:54:29 -0300

    Implemented main page header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,21 @@
 import './styles/global.css'
-import { Habit } from './components/habit/Habit'
 
-function App() {
-  return (
-    <div>
-      <Habit completed={3}/>
-      <Habit completed={6}/>
-      <Habit completed={9}/>
+import logoImage from './assets/logo.svg'
+
+export function App() {
+  return ( 
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoImage} alt="Habits" />
+          <button 
+            type='button'
+            className='border border-violet-500 font-semibold rounded-lg px-6 py-4'
+          >
+            Novo hábito
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
-
-export default App
