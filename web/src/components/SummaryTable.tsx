8d63cef157d363229209f91cedec1980698b9053
RCristyan <rcristyan9@gmail.com>
2023-01-27 19:28:16 -0300

    Adjusted weekday index in mapping

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -4,9 +4,9 @@ export function SummaryTable() {
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
-                {weekDays.map(weekday => {
+                {weekDays.map((weekday, index) => {
                     return (
-                        <div className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+                        <div key={`${weekday}-${index}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
                             {weekday}
                         </div>
                     )})
