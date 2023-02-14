c46cc76fcbae40304c5cdd42db0fdfce328defb0
RCristyan <rcristyan9@gmail.com>
2023-02-13 21:32:12 -0300

    Habit markers moved to HabitDay component

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,5 @@
+import { HabitDay } from "./HabitDay"
+
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 export function SummaryTable() {
@@ -14,17 +16,16 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <HabitDay />
+                <HabitDay />
+                <HabitDay />
+                <HabitDay />
+                <HabitDay />
+                <HabitDay />
+                <HabitDay />
+                <HabitDay />
+                <HabitDay />
+                <HabitDay />
             </div>
         </div>
     )
